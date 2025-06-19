a0ac427c007d447606fc0e7bb03b0a1c11f78920
CMDR_Hans <ha.araya@hotmail.com>
2025-06-19 12:45:07 -0400

    Final hecho

--- funciones.py
+++ funciones.py
@@ -1,9 +1,14 @@
+#agregar import necesarios
 import os,time
 
 def limpiar():
     """"Ejecuta un limpiar pantalla"""
     os.system("cls")
 
+#Variables por usar
+compras=[]
+tipos=('G','V')
+    
 def menu():
     menu="""MENU PRINCIPAL
 1. Comprar entrada
@@ -17,11 +22,11 @@ def menu():
         limpiar()
 
         if opc=="1":
-            pass
+            comprar_entrada()
         elif opc=="2":
-            pass
+            consultar_compra()
         elif opc=="3":
-            pass
+            cancelar_compra()
         elif opc=="4":
             print("Programa terminado...")
             break
@@ -29,4 +34,63 @@ def menu():
             print("Opción incorrecta")
         time.sleep(3)
 
+
+def comprar_entrada():
+    print("COMPRAR ENTRADA")
+    nombre=validar_nombre("Ingrese nombre del comprador: ")
+    tipoE=validar_tipo()
+    codigo=input("Ingrese código: ")
+
+    compra={
+        "nombre":nombre,
+        "tipo":tipoE,
+        "codigo":codigo
+    }
+    compras.append(compra)
+    print("¡Entrada registrada con éxito!")
+
+def consultar_compra():
+    print("CONSULTAR COMPRADOR")
+    nombre=validar_nombre("Ingrese nombre del comprador a buscar: ")
+    for c in compras:
+        if nombre==c["nombre"]:
+            print(f"Tipo de entrada: {c["tipo"]}, Código: {c["codigo"]}")
+            return
+    print("Nombre no registrado")        
+
+def cancelar_compra():
+    print("CANCELAR COMPRA")
+    nombre=validar_nombre("Ingrese nombre del comprador a cancelar compra: ")
+    for c in compras:
+        if nombre==c["nombre"]:
+            compras.remove(c)
+            print("¡Compra cancelada!")
+            return
+    print("Nombre no encontrado")
+
+
+#validaciones: nombre, codigo, tipo, lista no vacia
+    
+def validar_nombre(mensaje: str):
+    while True:
+        nombre=input(mensaje).strip().title()
+        if len(nombre)>=3 and nombre.isalpha():
+            return nombre
+        print("Error! el nombre mínimo 3 letras!")
+
+
+def validar_tipo():
+    while True:
+        tipo=input("Ingrese tio de entrada (G:general, V:Vip): ").upper()
+        if tipo in tipos:
+            return tipo
+        print("Error! El tipo de debe ser G(general) o V(vip)!")
+
+
+def validar_codigo():
+    pass
+def validar_lista_vacia():
+    pass
+def validar_nombre_existente():
+    pass
 
